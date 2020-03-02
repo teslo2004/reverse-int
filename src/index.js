4720cc4f50d5ec97bfaaff405addb93bffaa087c
Alexander Tesliuk <teslo2004@mail.ru>
2020-03-02 16:06:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
-}
+    var num = n.toString().split("").reverse();
+    if (num[num.length-1] == "-"){
+      var res = num.slice(0,-1);
+      return +res.join("")
+    }
+    return +num.join("");
+  }
